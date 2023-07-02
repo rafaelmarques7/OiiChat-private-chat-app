7ff7708660ba38b82d9536183bd3442fcd979667
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-02 22:12:15 +0100

    checkpoint - main content

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,10 +4,9 @@ export const Header = (props) => {
   return (
     <header id='header'>
       <div className='intro'>
-        <ParticlesBg type="circle" bg={{zIndex: 0, position:"absolute", top:0}} />
+        <ParticlesBg type="lines" bg={{zIndex: 0, position:"absolute", top:0}} />
         <div className='overlay'>
           <div className='container'>
-          
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
@@ -19,7 +18,7 @@ export const Header = (props) => {
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
                 >
-                  Learn More
+                  Start chatting!
                 </a>{' '}
               </div>
             </div>
