209c2579f89fbb2d0e63fc046739b26e77224826
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-07 23:14:39 +0100

    cicd

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,8 +3,8 @@ const { MongoClient } = require("mongodb");
 const URL_FRONTEND = "http://dq5rcunnxjcst.cloudfront.net/";
 
 const URL_DB =
-  process.env.URL_DATABASE ||
   "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
+// process.env.URL_DATABASE ||
 
 const client = new MongoClient(URL_DB);
 const dbName = "ChatApp";
