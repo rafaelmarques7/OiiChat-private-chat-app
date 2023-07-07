861d0e2eabe926b8ff50eb64fa090522d67b485c
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-07 18:36:53 +0100

    configure frontend env

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,7 +1,4 @@
 import { io } from "socket.io-client";
 import { URL_BACKEND } from "../config";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = URL_BACKEND;
-
-export const socket = io(URL);
+export const socket = io(URL_BACKEND);
