21bb34555d41646f7b94a51eb8a0829a2031ecdb
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-07 19:32:12 +0100

    update cicd

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+const url = process.env.URL_DATABASE;
+//  "mongodb://localhost:27017";
 // "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
 const client = new MongoClient(url);
-const dbName = "ChatApp";
+const dbName = process.env.DB_NAME;
 
 module.exports = {
   dbName,
