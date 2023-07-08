8b614675a6eca9c8416d4e65bc0af21a78b77505
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-08 16:43:32 +0100

    checkpoint

--- frontend/src/pages/PublicRooms.js
+++ frontend/src/pages/PublicRooms.js
@@ -1,32 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { URL_GET_PUBLIC_ROOMS } from "../config";
 import { Navigation } from "../components/navigation";
 
+const formatDate = (timestamp) => {
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  const date = new Date(timestamp);
+  return date.toLocaleDateString(undefined, options);
+};
+
+const RoomInfo = ({ room }) => {
+  const { _id, roomName, timestamp } = room;
+
+  return (
+    <div className="public-rooms-item">
+      <div className="public-rooms-item-date">
+        Created: {formatDate(timestamp)}
+      </div>
+      <div className="public-rooms-item-title">{roomName}</div>
+      <Link to={`/rooms/${_id}`}>
+        <button>> Enter Room</button>
+      </Link>
+    </div>
+  );
+};
+
 export const PagePublicRooms = () => {
+  const [rooms, setRooms] = useState([]);
+
   useEffect(() => {
-    console.log("running page load effect.");
-
     const fetchData = async () => {
-      const url = URL_GET_PUBLIC_ROOMS;
-
-      console.log("making GET request: ", url);
-      const response = await fetch(url, {
-        mode: "cors",
-      });
-      const data = await response.json();
-      console.log("fetch returned", { response, data });
+      try {
+        const response = await fetch(URL_GET_PUBLIC_ROOMS, { mode: "cors" });
+        const data = await response.json();
+        setRooms(data);
+      } catch (error) {
+        console.error("Error fetching public rooms:", error);
+      }
     };
 
     fetchData();
   }, []);
 
-  console.log("rendering public rooms page: ", {});
-
   return (
-    <div className="chatroom-container">
+    <>
       <Navigation />
+      <div className="public-rooms-container">
+        <div className="public-rooms-title">Public Rooms</div>
 
-      <p>Public rooms:</p>
-    </div>
+        <div className="public-rooms-list">
+          {rooms.map((room) => (
+            <RoomInfo key={room._id} room={room} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
