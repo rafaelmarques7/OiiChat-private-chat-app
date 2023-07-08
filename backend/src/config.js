162916a7a1776c2fe3d011964ede8c3ac2da093d
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-08 14:25:48 +0100

    update backend env

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,17 +1,20 @@
 const { MongoClient } = require("mongodb");
 
-const USE_CONFIG = process.env.USE_CONFIG || "prod";
-console.log("backend using config: ", USE_CONFIG);
+// const USE_CONFIG = process.env.USE_CONFIG || "prod";
+// console.log("backend using config: ", USE_CONFIG);
 
-const URL_FRONTEND =
-  USE_CONFIG === "dev"
-    ? "http://localhost:3000"
-    : "http://dq5rcunnxjcst.cloudfront.net";
+// const URL_FRONTEND =
+//   USE_CONFIG === "dev"
+//     ? "http://localhost:3000"
+//     : "http://dq5rcunnxjcst.cloudfront.net";
 
-const URL_DB =
-  USE_CONFIG === "dev"
-    ? "mongodb://localhost:27017"
-    : "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
+// const URL_DB =
+//   USE_CONFIG === "dev"
+//     ? "mongodb://localhost:27017"
+//     : "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
+
+const URL_FRONTEND = process.env.URL_FRONTEND;
+const URL_DB = process.env.URL_DB;
 
 const client = new MongoClient(URL_DB);
 const dbName = "ChatApp";
@@ -20,4 +23,5 @@ module.exports = {
   dbName,
   client,
   URL_FRONTEND,
+  URL_DB,
 };
