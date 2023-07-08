2fa727e57cb4367818ef537a07339c1b322c74bf
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-09 00:42:23 +0100

    update cdk stack

--- backend/cloud/cdk-stack.js
+++ backend/cloud/cdk-stack.js
@@ -31,60 +31,26 @@ class ChatAppCdkStack extends cdk.Stack {
       vpc: vpc,
     });
 
-    // Create an ALB
-    const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
-      vpc,
-      internetFacing: true,
-    });
-
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, "TargetGroup", {
-      vpc,
-      targetType: elbv2.TargetType.IP,
-      port: 80, // Adjust the port as needed
-      protocol: elbv2.ApplicationProtocol.HTTP,
-    });
-
-    const cert = acm.Certificate.fromCertificateArn(
-      this,
-      "Certificate",
-      "arn:aws:acm:eu-west-1:381336380362:certificate/6f944b07-a30e-439c-8da2-54890a644ec4"
-    );
-
-    const listener = alb.addListener("Listener", {
-      port: 443,
-      protocol: elbv2.ApplicationProtocol.HTTPS,
-      certificates: [cert],
-      defaultTargetGroups: [targetGroup],
-    });
-
-    // Create Fargate Service with a load balancer
     const fargateService =
       new ecs_patterns.ApplicationLoadBalancedFargateService(
         this,
         "FargateService",
         {
           cluster: cluster, // Required
-          targetGroup: targetGroup,
           cpu: 256, // Default is 256
           desiredCount: 1, // Default is 1
           taskImageOptions: {
             image: ecs.ContainerImage.fromRegistry(dockerImageAsset.imageUri),
             containerPort: 5001,
-            logDriver: new ecs.AwsLogDriver({
-              streamPrefix: "MyApp",
-              logGroup: new logs.LogGroup(this, "LogGroup", {
-                logGroupName: "/ecs/MyApp",
-                retention: logs.RetentionDays.ONE_WEEK,
-                removalPolicy: cdk.RemovalPolicy.DESTROY,
-              }),
-            }),
-            environment: {
-              URL_DATABASE: process.env.URL_DATABASE,
-              URL_FRONTEND: process.env.URL_FRONTEND,
-            },
           },
           memoryLimitMiB: 512, // Default is 512
           publicLoadBalancer: true, // Default is false
+          protocol: elbv2.ApplicationProtocol.HTTPS, // Use HTTPS for the listener
+          certificate: acm.Certificate.fromCertificateArn(
+            this,
+            "Certificate",
+            "arn:aws:acm:eu-west-1:381336380362:certificate/6f944b07-a30e-439c-8da2-54890a644ec4"
+          ),
         }
       );
 
