54e286d4296ff1f6f0c0152457a31ba0cf47bbc3
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-08 13:28:18 +0100

    trigger deploy

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,9 +1,16 @@
 const { MongoClient } = require("mongodb");
 
-const URL_FRONTEND = "http://dq5rcunnxjcst.cloudfront.net";
+const USE_CONFIG = "prod"; // "dev";
+
+const URL_FRONTEND =
+  USE_CONFIG === "dev"
+    ? "http://localhost:3000"
+    : "http://dq5rcunnxjcst.cloudfront.net";
 
 const URL_DB =
-  "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
+  USE_CONFIG === "dev"
+    ? "mongodb://localhost:27017"
+    : "mongodb+srv://admin:rEOsy9NyorTqZDpJ@chatapp.bm2144p.mongodb.net/";
 // process.env.URL_DATABASE ||
 
 const client = new MongoClient(URL_DB);
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,9 @@
+const USE_CONFIG = "prod"; // "dev";
+
 export const URL_BACKEND =
-  "http://chata-farga-1tcapa2a4psyy-692226949.eu-west-1.elb.amazonaws.com";
-//   process.env.REACT_APP_URL_BACKEND ||
+  USE_CONFIG === "dev"
+    ? "http://localhost:5001"
+    : "http://chata-farga-1tcapa2a4psyy-692226949.eu-west-1.elb.amazonaws.com";
 
 export const URL_MESSAGES = `${URL_BACKEND}/messages`;
 export const URL_MESSAGES_ROOM = `${URL_BACKEND}/messages/rooms`;
