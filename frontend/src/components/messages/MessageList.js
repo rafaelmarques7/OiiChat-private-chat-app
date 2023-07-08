7509b1cdcf20f3f6bb3c11a7a30eef416bebfe24
Rafael Marques <rafael.marques@CVX-0920.convexin.local>
2023-07-08 14:55:11 +0100

    default message in new room

--- frontend/src/components/messages/MessageList.js
+++ frontend/src/components/messages/MessageList.js
@@ -1,16 +1,28 @@
 import TextMessage from "./TextMessage";
 import { howLongAgo } from "./lib";
 
-export default ({ messages, userId }) => (
-  <>
-    {messages.map((message) => (
+export default ({ messages, userId }) => {
+  if (!messages || messages.length === 0)
+    return (
       <TextMessage
-        key={message?._id}
-        text={message?.text}
-        author={message?.username}
-        timestamp={howLongAgo(message?.timestamp)}
-        direction={message?.username === userId ? "outgoing" : "incoming"}
+        author={"Admin"}
+        timestamp={"just now"}
+        text="New messages will appear here"
+        direction="incoming"
       />
-    ))}
-  </>
-);
+    );
+
+  return (
+    <>
+      {messages.map((message) => (
+        <TextMessage
+          key={message?._id}
+          text={message?.text}
+          author={message?.username}
+          timestamp={howLongAgo(message?.timestamp)}
+          direction={message?.username === userId ? "outgoing" : "incoming"}
+        />
+      ))}
+    </>
+  );
+};
